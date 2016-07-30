f79a2300ed9028aa89aa27344b19e91856ebfb11
minkwon <s4286260@gmail.com>
2016-07-30 21:54:08 +1000

    implement toiletService

---
+++ api/BrisbaneLocateAPI/BikeRacks/Services/ToiletService.cs
@@ -0,0 +1,34 @@
+﻿using CsvIngestion.Mappings;
+using CsvIngestion.Models;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvIngestion.Services
+{
+	public class ToiletService
+	{
+		static public List<BikeRack> ToiletLocations { get; private set;}
+
+		public ToiletService(string location = "")
+		{
+			if (ToiletLocations == null)
+			{
+
+				if (location.Equals(""))
+				{
+					location = "/Static/ToiletmapExport.csv";
+				}
+
+				var ToiletLocationsCSV = new CsvReader(File.OpenText(location));
+				ToiletLocationsCSV.Configuration.RegisterClassMap<ToiletMap>();
+				ToiletLocations = ToiletLocationsCSV.GetRecords<Toilet>().ToList();
+			}
+		}
+
+	}
+}
--- api/BrisbaneLocateAPI/CityCycleTestApplication/Program.cs
+++ api/BrisbaneLocateAPI/CityCycleTestApplication/Program.cs
@@ -13,22 +13,6 @@ namespace CityCycleTestApplication
         {
             CityCycleService service = new CityCycleService();
             service.GetStations();
-
-			static public List<BikeRack> BikeRacks { get; private set;}
-
-			if (BikeRacks == null)
-			{
-
-				if (location.Equals(""))
-				{
-					location = "/Static/CBD-bike-racks.csv";
-				}
-
-				var BikeRackLocationsCSV = new CsvReader(File.OpenText(location));
-				BikeRackLocationsCSV.Configuration.RegisterClassMap<BikeRackMap>();
-				BikeRacks = BikeRackLocationsCSV.GetRecords<BikeRack>().ToList();
-			}
-
         }
     }
 }
