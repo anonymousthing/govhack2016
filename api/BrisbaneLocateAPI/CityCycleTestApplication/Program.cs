fc62b7627edb071ec1e5ce7006d37a9ffdf9059b
minkwon <s4286260@gmail.com>
2016-07-30 21:40:46 +1000

    add toilet model, toilet mapping

--- api/BrisbaneLocateAPI/CityCycleTestApplication/Program.cs
+++ api/BrisbaneLocateAPI/CityCycleTestApplication/Program.cs
@@ -13,6 +13,22 @@ namespace CityCycleTestApplication
         {
             CityCycleService service = new CityCycleService();
             service.GetStations();
+
+			static public List<BikeRack> BikeRacks { get; private set;}
+
+			if (BikeRacks == null)
+			{
+
+				if (location.Equals(""))
+				{
+					location = "/Static/CBD-bike-racks.csv";
+				}
+
+				var BikeRackLocationsCSV = new CsvReader(File.OpenText(location));
+				BikeRackLocationsCSV.Configuration.RegisterClassMap<BikeRackMap>();
+				BikeRacks = BikeRackLocationsCSV.GetRecords<BikeRack>().ToList();
+			}
+
         }
     }
 }
